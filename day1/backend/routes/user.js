ab682ed91df619b8de59c3f69314dbb544cc734c
arturfil <arturfil@hotmail.com>
2021-09-22 17:54:20 -0400

    added regular expression validation

--- day1/backend/routes/user.js
+++ day1/backend/routes/user.js
@@ -11,7 +11,10 @@ const { userSignUp, userLogin } = require('../controllers/userController');
 router.post('/signup', [
   check("name", "Name field is required").not().isEmpty(),
   check("email", "Must be a valid Email").isEmail(),
-  check("password", "Password must be 8 characters long").isLength({min: 8}),
+  check("password", "Password must be 8 characters long with capital letter & Symbol")
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+  // ( [a-z] [A-Z] ^[0-9a-zA-Z] {8,} )
+  // lowercase, uppercase, (is a character) => isNot => numbers&lowercase&uppercase, minLength = 8
   validateFields
 ] ,userSignUp);
 
